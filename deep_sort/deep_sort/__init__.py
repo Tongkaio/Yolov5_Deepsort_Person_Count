79192a063d3d75061cf998757c6d9da8ea205b7a
tongkai <1092019531@qq.com>
2024-05-23 09:34:10 +0000

    refine & add develop dockerfile

--- deep_sort/deep_sort/__init__.py
+++ deep_sort/deep_sort/__init__.py
@@ -1,7 +1,8 @@
 from .deep_sort import DeepSort
+from .deep_sort_trt import DeepSortTRT
 
 
-__all__ = ['DeepSort', 'build_tracker']
+__all__ = ['DeepSort', 'build_tracker', 'DeepSortTRT', 'build_tracker_trt']
 
 
 def build_tracker(cfg, use_cuda):
@@ -10,12 +11,9 @@ def build_tracker(cfg, use_cuda):
                 nms_max_overlap=cfg.DEEPSORT.NMS_MAX_OVERLAP, max_iou_distance=cfg.DEEPSORT.MAX_IOU_DISTANCE, 
                 max_age=cfg.DEEPSORT.MAX_AGE, n_init=cfg.DEEPSORT.N_INIT, nn_budget=cfg.DEEPSORT.NN_BUDGET, use_cuda=use_cuda)
     
-
-
-
-
-
-
-
-
+def build_tracker(cfg, use_cuda):
+    return DeepSortTRT(cfg.DEEPSORT.REID_CKPT, 
+                max_dist=cfg.DEEPSORT.MAX_DIST, min_confidence=cfg.DEEPSORT.MIN_CONFIDENCE, 
+                nms_max_overlap=cfg.DEEPSORT.NMS_MAX_OVERLAP, max_iou_distance=cfg.DEEPSORT.MAX_IOU_DISTANCE, 
+                max_age=cfg.DEEPSORT.MAX_AGE, n_init=cfg.DEEPSORT.N_INIT, nn_budget=cfg.DEEPSORT.NN_BUDGET, use_cuda=use_cuda)
 
